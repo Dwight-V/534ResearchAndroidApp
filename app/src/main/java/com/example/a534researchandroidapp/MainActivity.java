6dc007f8089c0ff3b28c0527a50c4c6864ed32ed
Dwight-V <dvight@duck.com>
2024-06-02 16:53:32 -0400

    Changed some variable names, added a check for btnStart

--- app/src/main/java/com/example/a534researchandroidapp/MainActivity.java
+++ app/src/main/java/com/example/a534researchandroidapp/MainActivity.java
@@ -32,14 +32,15 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnRandomInt, btnStart, btnStop, btnClear;
     private TextView txtInitialData;
-    private LineChart lineChartHistoric;
+    private LineChart lChartHistoric;
 
 
     private int xValCount = 0;
-    private List<Entry> lChartHistoricEntries = new ArrayList<Entry>();
+    private List<Entry> historicalEntries = new ArrayList<Entry>();
 
     private Handler handler = new Handler(Looper.getMainLooper());
     private Runnable runnable;
+    private boolean handlerIsRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +52,13 @@ public class MainActivity extends AppCompatActivity {
         btnStop = (Button) findViewById(R.id.btn_stop);
         btnClear = (Button) findViewById(R.id.btn_clear);
         txtInitialData = (TextView) findViewById(R.id.txt_initial_data);
-        lineChartHistoric = (LineChart) findViewById(R.id.lchart_historic);
+        lChartHistoric = (LineChart) findViewById(R.id.line_chart_historic);
 
         // Similar to a TimerTask for java.util.Timer; gets called ever TIME_INTERVAL.
         runnable = new Runnable() {
             @Override
             public void run() {
-                addRandomEntry();
+                addRandomEntryHistorical();
                 updateLChartHistorical();
                 handler.postDelayed(this, TIME_INTERVAL); // Schedule the runnable to run again after 1 second
             }
@@ -66,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         btnRandomInt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addRandomEntry();
+                addRandomEntryHistorical();
                 updateLChartHistorical();
             }
         });
@@ -74,8 +75,10 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Auto-
-                handler.post(runnable);
+                if (!handlerIsRunning) {
+                    handlerIsRunning = !handlerIsRunning;
+                    handler.post(runnable);
+                }
             }
         });
 
@@ -83,31 +86,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 handler.removeCallbacks(runnable);
+                handlerIsRunning = false;
             }
         });
 
         btnClear.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                lChartHistoricEntries.clear();
+                historicalEntries.clear();
                 updateLChartHistorical();
                 return false;
             }
         });
     }
 
-    public void addEntry(int toBeAdded) {
-        lChartHistoricEntries.add(new Entry(xValCount++, toBeAdded));
+    public void addEntryHistorical(int toBeAdded) {
+        historicalEntries.add(new Entry(xValCount++, toBeAdded));
     }
 
     public void updateLChartHistorical() {
         // .*DataSet puts your data together, which can then be formatted before display.
-        LineDataSet dataSet1 = new LineDataSet(lChartHistoricEntries, "");
+        LineDataSet dataSet1 = new LineDataSet(historicalEntries, "");
         dataSet1.setColor(Color.rgb(155, 0, 0));
         dataSet1.setCircleColor(Color.rgb(155,0,0));
         dataSet1.setDrawFilled(true);
         dataSet1.setFillColor(Color.rgb(100, 0, 0));
-        dataSet1.setDrawValues(false);
 
         // .*Data is an object for even further customizing your specific graph output.
         LineData lineData = new LineData(dataSet1);
@@ -118,26 +121,26 @@ public class MainActivity extends AppCompatActivity {
 //        LineData lineData = new LineData(dataSetFinal);
 
         // x-axis customizing
-        lineChartHistoric.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        lineChartHistoric.getXAxis().setTextSize(15); // The float represents pixel density, within [6f, 24f]
-        lineChartHistoric.getXAxis().setGranularity(1); // Sets the minimum step when zooming in
+        lChartHistoric.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lChartHistoric.getXAxis().setTextSize(15); // The float represents pixel density, within [6f, 24f]
+        lChartHistoric.getXAxis().setGranularity(1); // Sets the minimum step when zooming in
         // y-axis customizing
-        lineChartHistoric.getAxisRight().setDrawLabels(false); // Disables the desired y-axis
-        lineChartHistoric.getAxisLeft().setGranularity(1);
-        lineChartHistoric.getAxisLeft().setLabelCount(10);
-        lineChartHistoric.getAxisLeft().setTextSize(15);
+        lChartHistoric.getAxisRight().setDrawLabels(false); // Disables the desired y-axis
+        lChartHistoric.getAxisLeft().setGranularity(1);
+        lChartHistoric.getAxisLeft().setLabelCount(10);
+        lChartHistoric.getAxisLeft().setTextSize(15);
 
 
         // Finalizes and displays your data!
-        lineChartHistoric.setData(lineData);
-        lineChartHistoric.invalidate(); // refreshes the chart
+        lChartHistoric.setData(lineData);
+        lChartHistoric.invalidate(); // refreshes the chart
     }
 
-    public void addRandomEntry() {
+    public void addRandomEntryHistorical() {
         // Generates a int in the range [1, 10]
         int rand = (int) (Math.random() * MAX_RANDOM_INT + MIN_RANDOM_INT);
 
         txtInitialData.setText(Integer.toString(rand));
-        addEntry(rand);
+        addEntryHistorical(rand);
     }
 }
